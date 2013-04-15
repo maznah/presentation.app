24e5c709f2c84017a03a739ecc6a79028f082870
Maznah <e1242030@rmqkr.net>
2013-04-15 10:26:37 +0400

    updates

--- TouchPoint/gen/com/example/touchpoint/R.java
+++ TouchPoint/gen/com/example/touchpoint/R.java
@@ -29,13 +29,14 @@ public final class R {
     }
     public static final class id {
         public static final int RelativeLayout=0x7f0a0005;
-        public static final int menu_exit=0x7f0a0008;
-        public static final int menu_settings=0x7f0a0007;
+        public static final int menu_exit=0x7f0a0009;
+        public static final int menu_settings=0x7f0a0008;
         public static final int objectList=0x7f0a0001;
         public static final int pager=0x7f0a0002;
         public static final int pager_title_strip=0x7f0a0003;
         public static final int paint=0x7f0a0000;
         public static final int textView1=0x7f0a0006;
+        public static final int textView2=0x7f0a0007;
         public static final int viewer=0x7f0a0004;
     }
     public static final class layout {
@@ -50,6 +51,7 @@ public final class R {
     public static final class string {
         public static final int action_exit=0x7f070002;
         public static final int action_settings=0x7f070001;
+        public static final int app_copyright=0x7f07000b;
         public static final int app_name=0x7f070000;
         public static final int dlgCancel=0x7f070006;
         public static final int dlgExit=0x7f070005;
